82dcd1c5d142b06d1cf509a19ae03e9e032349d4
Milad Abooali <m.abooali@hotmail.com>
2022-02-13 17:36:49 +0300

    Fix

--- modules/gateways/IranMelliBank.php
+++ modules/gateways/IranMelliBank.php
@@ -3,16 +3,17 @@
  **************************************************************************
  * IranMelliBank Gateway
  * IranMelliBank.php
- * Meta Data, Config & Link
+ * Meta Data & Config & Link
  * @author           Milad Abooali <m.abooali@hotmail.com>
  * @version          1.0
  **************************************************************************
  * @noinspection PhpUnused
  * @noinspection PhpUndefinedFunctionInspection
+ * @noinspection PhpUndefinedMethodInspection
+ * @noinspection PhpDeprecationInspection
  * @noinspection SpellCheckingInspection
  * @noinspection PhpIncludeInspection
  * @noinspection PhpIncludeInspection
- **************************************************************************
  */
 
 /**
@@ -23,11 +24,11 @@ function IranMelliBank_MetaData()
 {
     return [
         "DisplayName"                   => "IranMelliBank",
-        "gatewayType"                   => "Bank", // Only set if the module is a Bank Module
-        "failedEmail"                   => "Credit Card Payment Failed",
-        "successEmail"                  => "Custom Credit Card Payment Template", // You can utilise custom templates here
-        "pendingEmail"                  => "Custom Credit Card Pending Template",
-        "APIVersion"                    => "1.1", // Use API Version 1.1
+        "gatewayType"                   => "Bank",
+        "failedEmail"                   => "Custom Payment_Failed",
+        "successEmail"                  => "Custom Payment_Success",
+        "pendingEmail"                  => "Custom Payment_Pending",
+        "APIVersion"                    => "1.1",
         "DisableLocalCreditCardInput"   => true,
         "TokenisedStorage"              => false
     ];
@@ -40,33 +41,27 @@ function IranMelliBank_MetaData()
 function IranMelliBank_config()
 {
     return [
-        "FriendlyName"       => ["Type" => "System", "Value" => "بانک ملی ایران"],
-
+        "FriendlyName" 			 => ["Type" => "System", "Value" => "بانک ملی ایران"],
         // Gateway Setup
-        "cb_gw_MerchantId"   => ["FriendlyName" => "کد پذيرنده", "Type" => "text", "Size" => "50"],
-        "cb_gw_TerminalId"   => ["FriendlyName" => "شماره ترمینال", "Type" => "text", "Size" => "50"],
-        "cb_gw_TerminalKey"  => ["FriendlyName" => "کلید تراکنش", "Type" => "text", "Size" => "50"],
-        "cb_gw_unit"         => ["FriendlyName" => "واحد پول سیستم", "Type" => "dropdown", "Options" => ["1" => "ریال", "10" => "تومان"],"Description" => "لطفا واحد پول سیستم خود را انتخاب کنید."],
-
+        "cb_gw_method"           => ["FriendlyName" => "روش اتصال", "Type" => "radio", "Options" => "Token,Old"],
+        "cb_gw_id"               => ["FriendlyName" => "کد پذيرنده", "Type" => "text", "Size" => "50"],
+        "cb_gw_pass"             => ["FriendlyName" => "رمز پذيرنده", "Type" => "text", "Size" => "50"],
+        "cb_gw_unit"             => ["FriendlyName" => "واحد پول سیستم", "Type" => "dropdown", "Options" => ["1" => "ریال", "10" => "تومان"],"Description" => "لطفا واحد پول سیستم خود را انتخاب کنید."],
         // Email Notification
         "cb_email"               => ["FriendlyName" => "Email Notify", "Type" => "", "Description" => "<i class='far fa-envelope'></i> ارسال هشدار پرداخت به پست‌الکترونیک"],
         "cb_email_on_success"    => ["FriendlyName" => "On Success", "Type" => "dropdown", "Options" => ["0" => "خیر", "1" => "بله"],"Description" => "ارسال هشدار تراکنش موفق به پست‌الکترونیک"],
         "cb_email_on_error"      => ["FriendlyName" => "On Failed ", "Type" => "dropdown", "Options" => ["0" => "خیر", "1" => "بله"], "Description" => "ارسال هشدار تراکنش ناموفق به پست‌الکترونیک"],
-        "cb_email_from"          => ["FriendlyName" => "Email addresses", "Type" => "text", "Value"=>"info@codebox.ir", "Description" => "جهت اطمینان از دریافت ایمیل حتما آدرس واقعی مستقر روی همین سرور را استفاده کنید."],
-        "cb_email_address"       => ["FriendlyName" => "Email addresses", "Type" => "text", "Description" => "جهت جداسازی از کاراکتر , استفاده کنید."],
-
+        "cb_email_from"          => ["FriendlyName" => "Email From", "Type" => "text", "Value"=>"info@example.ir", "Description" => "جهت اطمینان از دریافت ایمیل حتما آدرس واقعی مستقر روی همین سرور را استفاده کنید."],
+        "cb_email_address"       => ["FriendlyName" => "Email TO", "Type" => "text", "Description" => "جهت جداسازی از کاراکتر , استفاده کنید."],
         // Telegram Notification
-        "cb_telegram"            => ["FriendlyName" => "Telegram Notify", "Type" => "", "Description" => "<img src='//t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://telegram.org' alt='T'> ارسال هشدار پرداخت به تلگرام"],
+        "cb_telegram"            => ["FriendlyName" => "Telegram Notify", "Type" => "", "Description" => "<img alt='Telegram' src='https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://telegram.org'> ارسال هشدار پرداخت به تلگرام"],
         "cb_telegram_on_success" => ["FriendlyName" => "On Success", "Type" => "dropdown", "Options" => ["0" => "خیر", "1" => "بله"],"Description" => "ارسال هشدار تراکنش موفق به تلگرام"],
         "cb_telegram_on_error"   => ["FriendlyName" => "On Failed", "Type" => "dropdown", "Options" => ["0" => "خیر", "1" => "بله"], "Description" => "ارسال هشدار تراکنش ناموفق به تلگرام"],
         "cb_telegram_chatid"     => ["FriendlyName" => "Telegram Chat Id", "Type" => "text", "Description" => "شناسه چت مقصد"],
         "cb_telegram_bot"        => ["FriendlyName" => "Telegram Bot Token", "Type" => "text", "Description" => "توکن ربات تلگرام"],
-
-        // Copyright & Brand Link
-        /*
-         "cb_copyright"    => ["FriendlyName" => "حق نشر", "Type" => "", "Description" => "Copyright (c) <a href='https://codebox.ir' target='_blank' style='color:#00741b'>Codebox</a> - 2022"]
-         */
-        "cb_copyright"    => ["FriendlyName" => "حق نشر", "Type" => "", "Description" => "Copyright (c) <a href='https://whmcsco.ir' target='_blank'>مجموعه WHMCS فارسی</a>"]
+        // Copyright & Branding Link
+        # "cb_copyright"    => ["FriendlyName"  => "حق نشر", "Type" => "", "Description" => "Copyright (c) <a href='https://codebox.ir' target='_blank' style='color:#00741b'>Codebox</a> - 2022"],
+        "cb_copyright"          => ["FriendlyName"  => "حق نشر", "Type" => "", "Description" => "Copyright (c) <a href='https://whmcsco.ir' target='_blank'>مجموعه WHMCS فارسی</a>"]
     ];
 }
 
@@ -77,10 +72,13 @@ function IranMelliBank_config()
  */
 function IranMelliBank_link($params)
 {
-    return '<form method="post" action="modules/gateways/IranMelliBank/payment.php?a=send">
+    $gateway_name = 'IranMelliBank';
+    $amount_Rial  = round(($params['amount']-'.00') * $params['cb_gw_unit']);
+    return '<form method="post" action="modules/gateways/'.$gateway_name.'/payment.php?a=send">
 	<input type="hidden" name="invoiceid" value="'.$params['invoiceid'].'">
-	<input type="hidden" name="amount" value="'.round(($params['amount']-'.00') * $params['cb_gw_unit']).'">
-	<input type="hidden" name="email" value="'.$params['clientdetails']['email']. '">
+	<input type="hidden" name="amount" value="'.$amount_Rial.'">
+	<input type="hidden" name="email" value="'.$params['clientdetails']['email'].'">
 	<input type="submit" name="pay" value="پرداخت"></form>
-	<img src="/modules/gateways/IranMelliBank/logo.png" alt="Melli Bank" style="max-width:170px;height:45px;">';
+	<img src="/modules/gateways/'.$gateway_name.'/logo.png" alt="'.$gateway_name.'" style="max-width:170px;height:45px;">';
 }
+
